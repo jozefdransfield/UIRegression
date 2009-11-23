585e8e40a0f4814e9cc478cba39c8f94c4120c47
Jozef Dransfield <Jozef.Dransfield@me.com>
2009-11-23 19:18:35 +0000

    Now dependant on selenium-rc

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -6,20 +6,20 @@ eventAllTestsStart = {
   }
 }
 
-eventTestPhaseStart = {phase ->
-  if (phase == 'functional') {
-    event("StatusUpdate", ['Starting Selenium RC'])
-    def managerClass = Thread.currentThread().contextClassLoader.loadClass("grails.plugins.selenium.SeleniumManager")
-    seleniumManager = managerClass.instance
-    seleniumManager.loadConfig()
+eventTestSuiteStart = {String type ->
+	if (type == "ui-regression-test") {
+		def managerClass = Thread.currentThread().contextClassLoader.loadClass("grails.plugins.selenium.SeleniumManager")
+		seleniumManager = managerClass.instance
+		seleniumManager.loadConfig()
 
-    seleniumManager.startServer("${seleniumRcPluginDir}/lib/server/selenium-server.jar")
-  }
+		event("StatusUpdate", ["starting selenium server"])
+		seleniumManager.startServer("${seleniumRcPluginDir}/lib/server/selenium-server.jar")
+	}
 }
 
-eventTestPhaseEnd = {phase ->
-  if (phase == 'functional') {
-    event("StatusUpdate", ['Stopping Selenium RC'])
-    seleniumManager.stopServer()
-  }
+eventTestSuiteEnd = {String type, testSuite ->
+	if (type == "ui-regression-test") {
+		event("StatusUpdate", ["stopping selenium server"])
+		seleniumManager.stopServer()
+	}
 }
